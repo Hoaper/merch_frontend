8542581add32f9e6e7fe1eaf0fc860a479ca9ffd
Hoaper <beeboplay@gmail.com>
2024-02-03 16:11:32 +0600

    fixed response

--- app/api/get_items/route.ts
+++ app/api/get_items/route.ts
@@ -1,14 +1,13 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import connectDB from "@/lib/db";
 import Items from "@/models/items";
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
     await connectDB();
     try {
         const items = await Items.find({});
         return NextResponse.json({items: items}, {status: 200});
     } catch (error) {
         console.error('Error fetching items:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        NextResponse.json({ error: 'Internal Server Error' }, {status: 200});
     }
 }
